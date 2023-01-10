d1a6c451554030651eeb935275c437cb5b708162
Wavened <51140407+MrJyle@users.noreply.github.com>
2023-01-10 20:11:14 +0000

    Update Module 2 Content

--- Module 2/Variables&DataTypes.py
+++ Module 2/Variables&DataTypes.py
@@ -1,18 +1,12 @@
 '''
 Data Types you can find in python are:
 
-    - 123  | Integers
-    - 1.23 | Floats
-    - True, False | Booleans
-    - "Hello, world! | Strings
-    - [1,2,3,4.0,"5",True] | Lists
-    
-    - {
-        "name": "St. Mary's",
-        "occupation": "Coding", | Dictionaries
-        "organization: "Club"
-       }
-
+    - Integers
+    - Floats
+    - Booleans
+    - Strings
+    - Lists
+    - Dictionaries
     - Classes and Objects
 
 These are just the types we will be exploring. There are alot of types in python like bytes (b'') but 
@@ -20,3 +14,24 @@ we won't cover them because they too complicated for now. If you are curious, yo
 https://docs.python.org/3/library/stdtypes.html
 for a full documentation page on all the standard types and their functionality in Python.
 '''
+
+
+Integer = 10 # A whole number
+
+Float = 21.4 # A decimal number
+
+Boolean = True # A True or False | 1 or 0
+
+String = "Hello world!" # A line of text 
+
+List = [1, True, "Wow!", 2.44] # A list of data
+
+Dictionary = {
+        "name": "St. Mary's",
+        "occupation": "Coding", # A way to store data in a key:value system.
+        "organization": "Club" 
+       }
+
+       
+
+'''Classes / Objects will be explained in a later module.'''
